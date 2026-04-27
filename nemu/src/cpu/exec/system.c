bc2af65a48331e6c4d468aae86d4e32ae7023a03
tracer-ics2017 <tracer@njuics.org>
2026-04-27 04:55:12 -0400

    >  compile 2412737 lumine Linux Lemonade 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  04:55:12 up  3:58,  1 user,  load average: 0.07, 0.06, 0.08 80997fefaf013045169efbbc56796ddca3e11da

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -4,14 +4,30 @@ void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 void raise_intr(uint8_t NO, vaddr_t ret_addr);
 
-make_EHelper(lidt) {
-  vaddr_t addr = id_dest->addr;
+make_EHelper(lidt)
+{
+  vaddr_t addr;
+
+  if (id_dest->type == OP_TYPE_MEM)
+  {
+    addr = id_dest->addr;
+  }
+  else if (id_dest->type == OP_TYPE_REG)
+  {
+    // 对于 lidt (%reg) 这种形式，如果 decode_op_rm 没有正确给出 addr，
+    // 这里用寄存器内容作为内存地址
+    addr = reg_l(id_dest->reg);
+  }
+  else
+  {
+    panic("invalid operand type for lidt");
+  }
 
   cpu.idtr.limit = vaddr_read(addr, 2);
   cpu.idtr.base = vaddr_read(addr + 2, 4);
 
-  Log("lidt: addr=0x%x, idtr.base=0x%x, idtr.limit=0x%x",
-      addr, cpu.idtr.base, cpu.idtr.limit);
+  Log("lidt: type=%d, reg=%d, addr=0x%x, idtr.base=0x%x, idtr.limit=0x%x",
+      id_dest->type, id_dest->reg, addr, cpu.idtr.base, cpu.idtr.limit);
 
   print_asm_template1(lidt);
 }
